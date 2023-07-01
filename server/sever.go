cc778b4bf0d835dd0b7b2c1d86ee37aaca105d3f
GShaKi <65718150+GShaKi@users.noreply.github.com>
2023-07-01 21:39:00 +0330

    Fixed some bugs and added server's GetRate function

--- server/sever.go
+++ server/sever.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+
 	"github.com/hashicorp/go-hclog"
 
 	protos "github.com/DShaKi/Currency-API/protos/currency"
@@ -13,4 +15,10 @@ type Currency struct {
 
 func NewCurrency(l hclog.Logger) *Currency {
 	return &Currency{l, protos.UnimplementedCurrencyServer{}}
+}
+
+func (c *Currency) GetRate(ctx context.Context, rq *protos.RateRequest) (*protos.RateResponse, error) {
+	c.log.Info("Handle GetRate", "base", rq.GetBase(), "destination", rq.GetDestination())
+
+	return &protos.RateResponse{Rate: 0.5}, nil
 }
